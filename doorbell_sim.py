988c2e44c6fd6f8632915bf09d6edf0cad25f306
Steve Cline <steve.cline@weldre4.org>
2024-03-13 12:26:42 -0600

    Create doorbell_sim.py

---
+++ doorbell_sim.py
@@ -0,0 +1,15 @@
+#This script asks the user to input the minimum and maximum number of seconds between doorbell rings. It then simulates a doorbell ringing at random intervals between the minimum and maximum number of seconds. The script will run until the user presses Ctrl-C to stop it.
+
+import time
+import random
+import vlc
+p = vlc.MediaPlayer("D:\Documents\PyProjects\Colab-Sandbox\doorbell_sound.mp3")
+
+def doorbell_sim():
+    min_time = int(input("Enter the minimum number of seconds between doorbell rings: "))
+    max_time = int(input("Enter the maximum number of seconds between doorbell rings: "))
+    while True:
+        print("Ding dong!")
+        p.play()
+        time.sleep(random.randint(min_time, max_time))
+        p.stop()
